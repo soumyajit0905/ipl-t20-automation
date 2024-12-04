cd146c68c97bca584120b9a2ac465e0555ea1695
Soumyajit <soumyajit443@gmail.com>
2024-12-04 19:44:18 +0530

    renamed Test class

--- src/test/java/uiautomation/iplt20/com/IPLPointsTableTest.java
+++ src/test/java/uiautomation/iplt20/com/IPLPointsTableTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class IPLPointsTable extends BaseTest {
+public class IPLPointsTableTest extends BaseTest {
 
 	@Test
 	public void extractPointsTableData() {
